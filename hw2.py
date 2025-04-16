9cb496e6354885d0ac4ccfd5a58388e439afb71e
rameldawood2025 <rdawood1@depaul.edu>
2025-04-16 10:35:13 -0500

    Add Form inputs and safe index checks for POST and DELETE endpoints

--- hw2.py
+++ hw2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
@@ -6,52 +6,53 @@ users = []
 workouts = []
 meals = []
 
-# USERS 
+# USERS
 
 @app.get("/users")
 async def get_users():
     return {"users": users}
 
 @app.post("/users")
-async def add_user(name: str = ""):
+async def add_user(name: str = Form(...)):
     users.append(name)
     return {"users": users}
 
 @app.delete("/users")
 async def delete_user(index: int = 0):
-    users.pop(index)
+    if 0 <= index < len(users):
+        users.pop(index)
     return {"users": users}
 
-
-# WORKOUTS 
+# WORKOUTS
 
 @app.get("/workouts")
 async def get_workouts():
     return {"workouts": workouts}
 
 @app.post("/workouts")
-async def add_workout(name: str = ""):
+async def add_workout(name: str = Form(...)):
     workouts.append(name)
     return {"workouts": workouts}
 
 @app.delete("/workouts")
 async def delete_workout(index: int = 0):
-    workouts.pop(index)
+    if 0 <= index < len(workouts):
+        workouts.pop(index)
     return {"workouts": workouts}
 
-
-# MEALS 
+# MEALS
 
 @app.get("/meals")
 async def get_meals():
     return {"meals": meals}
 
 @app.post("/meals")
-async def add_meal(name: str = ""):
+async def add_meal(name: str = Form(...)):
     meals.append(name)
     return {"meals": meals}
 
 @app.delete("/meals")
 async def delete_meal(index: int = 0):
-    meals.pop(index)
+    if 0 <= index < len(meals):
+        meals.pop(index)
     return {"meals": meals}
